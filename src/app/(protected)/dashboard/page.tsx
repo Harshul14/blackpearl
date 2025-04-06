f6459be0967e820424218582be451536ea31127f
Harshul14 <harshul147@gmail.com>
2025-04-06 14:54:41 +0530

    setting up meeting card and firebase

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -5,12 +5,12 @@ import { ExternalLink, Github } from "lucide-react";
 import Link from "next/link";
 import CommitLog from "./commit-log";
 import AskQuestionCard from "./ask-question-card";
+import MeetingCard from "./meeting-card";
 
 const DashboardPage = () => {
   const { project } = useProject();
   return (
     <div>
-      {project?.id}
       <div className="flex flex-wrap items-center justify-between gap-y-4">
         {/* Github Link */}
         <div className="bg-primary w-fit rounded-md px-4 py-3">
@@ -38,7 +38,7 @@ const DashboardPage = () => {
       <div className="mt-4">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
           <AskQuestionCard />
-          MeetingsCard
+          <MeetingCard />
         </div>
         <div className="mt-8"></div>
         <CommitLog />
---
+++ src/lib/firebase.ts
@@ -0,0 +1,74 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRv28chPln43iXgviHCC45cylNDbIA_OQ",
+  authDomain: "blackpearl-b09cf.firebaseapp.com",
+  projectId: "blackpearl-b09cf",
+  storageBucket: "blackpearl-b09cf.firebasestorage.app",
+  messagingSenderId: "64158028801",
+  appId: "1:64158028801:web:3f3f113b8c9bdff6715c00",
+};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAs5SOHVdDXDwjzLhFXRNQONTc_0Q3L1qM",
+//     authDomain: "dionysus-yt.firebaseapp.com",
+//     projectId: "dionysus-yt",
+//     storageBucket: "dionysus-yt.firebasestorage.app",
+//     messagingSenderId: "294574424066",
+//     appId: "1:294574424066:web:9f028f2958cf265377a71b"
+// };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFile(
+  file: File,
+  setProgress?: (progress: number) => void,
+) {
+  return new Promise((resolve, reject) => {
+    try {
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+          );
+          if (setProgress) setProgress(progress);
+
+          switch (snapshot.state) {
+            case "paused":
+              console.log("upload is paused");
+              break;
+            case "running":
+              console.log("upload is running");
+              break;
+          }
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+            resolve(downloadUrl as string);
+          });
+        },
+      );
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
+}
