46861c6528a6f33eb7a820dfcfc776981626acc3
Harshul14 <harshul147@gmail.com>
2025-04-22 22:41:56 +0530

    adding styling in signIn and signUp page

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,13 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="text-foreground flex min-h-screen items-center justify-center bg-gradient-to-b from-[#0f172a] via-[#1e293b] to-[#0f172a]">
+      <div className="w-full max-w-md px-4 sm:px-6 lg:px-8">
+        <div className="glassmorphism rounded-2xl p-6 shadow-xl">
+          <SignUp />
+        </div>
+      </div>
+    </div>
+  );
 }
